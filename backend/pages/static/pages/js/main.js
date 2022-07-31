77c453cd46048f08d25f7420a7a7db8732c3087f
Ocean Winckler <oceanwinckler.rem@gmail.com>
2022-07-31 19:23:12 +0200

    homepage animations

--- backend/pages/static/pages/js/main.js
+++ backend/pages/static/pages/js/main.js
@@ -1,6 +1,8 @@
 // const sections = $("section");
 const sections = document.querySelectorAll("section");
 const navItems = document.querySelectorAll(".nav-container nav ul li a");
+const features = document.querySelectorAll(".feature");
+const featureLinks = document.querySelectorAll("#features .nav ul li");
 
 $(window).scroll(() => {
 	if ($(window).scrollTop() >= 130) {
@@ -9,18 +11,37 @@ $(window).scroll(() => {
 		$("body").removeClass("scroll");
 	}
 
-	let current = "";
+	let currentSection = "";
 	sections.forEach((section) => {
 		const sectionTop = section.offsetTop;
 		if ($(window).scrollTop() >= sectionTop) {
-			current = section.getAttribute("id");
+			currentSection = section.getAttribute("id");
 		}
 	});
 
 	navItems.forEach((item) => {
 		const section = item.getAttribute("data-section");
-		if (section == current) {
+		if (section == currentSection) {
 			item.classList.add("active");
+		} else {
+			item.classList.remove("active");
+		}
+	});
+
+	let currentFeature = "";
+	features.forEach((feature) => {
+		const featureTop = feature.offsetTop;
+		const feauteNavTop = document.querySelector("#features .nav").offsetTop + 120;
+		if (feauteNavTop >= featureTop) {
+			currentFeature = feature.getAttribute("id");
+		}
+	});
+
+	featureLinks.forEach((item) => {
+		const link = item.firstElementChild.getAttribute("href").replace("#", "");
+		if (link == currentFeature) {
+			item.classList.add("active");
+			found = true;
 		} else {
 			item.classList.remove("active");
 		}
