2a63ef3fc0d42bbdabb819bb90dd305b1c5f3059
Ocean Winckler <oceanwinckler.rem@gmail.com>
2022-08-03 14:39:50 +0200

    dashboard layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
 // Modules
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components
-import Sidebar from "./components/Sidebar";
+import Sidebar, { SidebarItem, SidebarGroup } from "./components/Sidebar";
 
 function App() {
 	return (
-		<>
-			<Sidebar title="Avocado" />
-		</>
+		<Router>
+			<Sidebar title="Avocado">
+				<SidebarItem label="Inbox" target="/" />
+				<SidebarGroup name="Collections">
+					<SidebarItem label="Fun" target="/collection/fun" />
+					<SidebarItem label="Dev" target="/collection/dev" />
+					<SidebarItem label="Cooking" target="/collection/cooking" />
+				</SidebarGroup>
+			</Sidebar>
+
+			<div className="main-content">
+				<Routes>
+					<Route path="/" element="inbox" />
+					<Route path="/collection/:name" element="collection" />
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,18 +1,42 @@
 // Modules
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 // Style
 import "../assets/styles/sidebar.scss";
 
-function Sidebar({ title }) {
+export default function Sidebar({ title, children }) {
 	return (
 		<div className="sidebar">
-			<div class="sidebar__head">
-				<div class="sidebar__title">{title}</div>
+			<div className="sidebar__head">
+				<div className="sidebar__title">{title}</div>
 			</div>
-			<div class="sidebar__body"></div>
+			<div className="sidebar__body">{children}</div>
 		</div>
 	);
 }
 
-export default Sidebar;
+export function SidebarGroup({ name, dropdown, children }) {
+	const [toggled, setToggled] = useState(false);
+
+	return (
+		<div className="sidebar__group">
+			<div
+				className={"sidebar__group__head" + (toggled ? " active" : "")}
+				onClick={() => setToggled(!toggled)}
+			>
+				<div className="sidebar__group__name">{name}</div>
+				<div className="sidebar__group__expand material-icons">expand_more</div>
+			</div>
+			<div className={"sidebar__group__items" + (toggled ? " expanded" : "")}>{children}</div>
+		</div>
+	);
+}
+
+export function SidebarItem({ label, target }) {
+	return (
+		<Link to={target}>
+			<div className="sidebar__item">{label}</div>
+		</Link>
+	);
+}
